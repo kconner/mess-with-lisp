57a3dc8b072f252eead9d0892397954efc56a242
Kevin Conner <connerk@gmail.com>
2022-09-18 16:03:16 -0700

    SICP 2.3.3 part 2

--- sicp/2.3.3.scm
+++ sicp/2.3.3.scm
@@ -76,3 +76,12 @@
 	  (entry set)
 	  (left-branch set)
 	  (adjoin-set x (right-branch set))))))
+
+(define (lookup given-key set-of-records)
+  (cond ((null? set-of-records) false)
+	((equal? given-key
+		 (key (car set-of-records)))
+	 (car set-of-records))
+	(else
+	 (lookup given-key
+		 (cdr set-of-records)))))

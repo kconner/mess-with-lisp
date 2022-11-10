6531af508a4c4ec372652b2e1c89826bbdc12049
Kevin Conner <connerk@gmail.com>
2022-11-09 23:12:13 -0800

    SICP 3.2.3

---
+++ sicp/3.2.3.scm
@@ -0,0 +1,10 @@
+(define (make-withdraw balance)
+  (lambda (amount)
+    (if (>= balance amount)
+	(begin (set! balance
+		     (- balance amount))
+	       balance)
+	"Insufficient funds")))
+
+(define W1 (make-withdraw 100))
+(W1 50)

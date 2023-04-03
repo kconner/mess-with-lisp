428ca09296ee7de36a23daba0ed442adaa745bad
Kevin Conner <connerk@gmail.com>
2023-04-02 18:23:24 -0700

    Touretzky chater 7

---
+++ touretzky/7.lisp
@@ -0,0 +1,23 @@
+;; map
+mapcar
+map
+
+;; filter
+remove-if-not
+remove-if
+
+;; reduce
+reduce
+
+;; find
+find
+find-if
+find-if-not
+
+;; aggregate logical
+and
+every
+notevery
+or
+some
+notany

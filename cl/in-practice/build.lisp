9e854fa67754729845869b144c22f4486ee98185
Kevin Conner <connerk@gmail.com>
2023-10-22 21:51:53 -0700

    Common Lisp in Practice version 1

---
+++ cl/in-practice/build.lisp
@@ -0,0 +1,6 @@
+(load "hello.lisp")
+
+(sb-ext:save-lisp-and-die
+  "hello"
+  :toplevel 'hello:main
+  :executable t)
---
+++ cl/in-practice/hello.lisp
@@ -0,0 +1,15 @@
+(defpackage :hello
+  (:use :common-lisp)
+  (:export :greet :main))
+
+(in-package :hello)
+
+(defun greet (whom)
+  "Create a greeting message for WHOM."
+  (format nil "Hello, ~A." whom))
+
+(defun main ()
+  "Greet someone, or something."
+  (write-line (greet (first (uiop:command-line-arguments))))
+  
+  (uiop:quit))
